23db6579c55f9fb17b75efe242a8a6d8471e0b1f
Dimly82 <pdimochka@gmail.com>
2022-03-31 15:06:00 +0300

    navbar + ico

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "The best web project in the world!"
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
