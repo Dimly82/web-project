5e23a5cd1e47d148d64638b77bde4e5f7a50d464
Dimly82 <pdimochka@gmail.com>
2022-04-04 18:47:23 +0300

    smth

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 
-from data import db_session
-from data.register_form import RegisterForm
-from data.__all_models import *
-
-
 from flask import Flask, render_template, request, redirect
+
+from data import db_session
+from data.__all_models import *
+from data.register_form import RegisterForm
 
 app = Flask(__name__)
 
@@ -46,7 +45,10 @@ def reqister():
         return redirect('/login')
     return render_template('register_form.html', title='Регистрация', form=form)
 
+
 if __name__ == '__main__':
+    SECRET_KEY = os.urandom(32)
+    app.config['SECRET_KEY'] = SECRET_KEY
     db_session.global_init("db/users.db")
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
