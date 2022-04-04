f4c734ddb8c5097cdd9f55e856fe2bd5a34c4fae
Student <mikhaylov2013a@yandex.ru>
2022-04-04 18:39:19 +0300

    ++

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
+
+from werkzeug.utils import redirect
+
 from data import db_session
+from data.register_form import RegisterForm
+from data.__all_models import *
 
 
 from flask import Flask, render_template
@@ -12,10 +17,29 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/register")
-def register():
-    return render_template("register_form.html")
-
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 if __name__ == '__main__':
     db_session.global_init("db/users.db")
