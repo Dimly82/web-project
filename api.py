f8d7d942ca6110c9280d83360ddcb75296ca1c0b
Dimly82 <pdimochka@gmail.com>
2022-04-18 19:02:58 +0300

    player stats

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ API = "https://ow-api.com/v1/stats/pc/eu/"
 
 def get_stats(battletag):
     response = requests.get(f"{API}/{battletag}/profile")
+    print(response.url)
     if not response:
         return False
-    print(response.url)
     return response.json()
 
